8b03769de5efd0203cdfb8db4e95724978aecfd5
Amaury Brito <amaurypb845@gmail.com>
2026-03-24 00:02:22 -0400

    mudanças no hero

--- src/componentes/Navbar.tsx
+++ src/componentes/Navbar.tsx
@@ -3,7 +3,10 @@ import "../estilos/navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Amaury.dev</div>
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+        <span>Amaury.dev</span>
+      </div>
 
       <div className="menu">
         <a href="#projetos">Projetos</a>
